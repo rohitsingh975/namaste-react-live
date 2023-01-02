eae33679fee19712f72f076b314e9690f2a10e41
Rohit Singh <engineer.rohit8@gmail.com>
2023-01-02 22:18:54 +0530

    Chapter 02 - Igniting our app

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
+/**
+ * HMR - Hot module replacement
+ * File Watcher Algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code
+ * Dev and Production Build
+ * Super fast build Algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatible with older version of browser - Add polyfills
+ * HTTPS on dev
+ * Port number 
+ * Consistent Hashing Algorithm
+ * Zero Config
+ * Transitive Dependencies
+ */
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const heading = React.createElement("h1",
 {
     id:"title"
 },
-"Heading 1");
+"Heading 1 from parcel");
 
 const heading2 = React.createElement("h2",
 {
